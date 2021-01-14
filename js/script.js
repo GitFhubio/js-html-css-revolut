9f83132b415ebea77cfe7da56c8b7f75f6cda308
Fabio Petrone <fa610movic90@gmail.com>
2021-01-14 17:23:41 +0100

    riflessione su click vs mouseenter mouseleave

--- js/script.js
+++ js/script.js
@@ -5,16 +5,43 @@
 // });
 
 $(document).ready(function() {
+    //
+    // $('.dropdown-item').click(function() {
+    //     var sottomenu = $(this).find('.dropdown-menu');
+    //     if(sottomenu.is(':visible')) {
+    //         sottomenu.hide();
+    //     } else {
+    //         $('.dropdown-menu').hide();
+    //         sottomenu.show();
+    //     }
+    //   });
+    //
 
-    $('.dropdown-item').click(function() {
+// il click fa un pò brutto anche se ho messo di chiudere tutti i drop aperti prima di aprirne uno nuovo,proprio a livello di esperienza visitatore penso, passo a mouseenter mouseleave 
+
+    $('.dropdown-item').mouseenter(function() {
         var sottomenu = $(this).find('.dropdown-menu');
-        if(sottomenu.is(':visible')) {
-            sottomenu.hide();
-        } else {
-            $('.dropdown-menu').hide();
+
             sottomenu.show();
-        }
+
       });
+      $('.dropdown-item').mouseleave(function() {
+          var sottomenu = $(this).find('.dropdown-menu');
+
+              sottomenu.hide();
+
+        });
+      // $('.dropdown-item').mouseleave(function() {
+      //     var sottomenu = $(this).find('.dropdown-menu');
+      //     if(sottomenu.is(':visible')) {
+      //         sottomenu.hide();
+      //     } else {
+      //         $('.dropdown-menu').hide();
+      //         sottomenu.show();
+      //     }
+      //   });
+
+// Per spegnere e accendere l'hamburger menu
       $('.burger-menu-icon').click(function() {
         $('#hamburger-menu').addClass('active');
       });
